f24d20be509c64f694d50ff08a976c13f24d7ca0
JuanjoSG5 <113671548+JuanjoSG5@users.noreply.github.com>
2025-04-25 16:11:26 +0200

    feat: small changes for deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,12 @@ import ScraperForm from '@/src/components/scrapForm';
 export default function Home() {
   return (
     <main>
-      <h1>AI Chatbot with RAG</h1>
+      <h1>Customizable Chatbot with RAG</h1>
+      <p>
+        The goal of this project is to create a chatbot that is easy to use
+        locally, and gives good results when asking for information about any
+        page. 
+      </p>
       <ScraperForm />
       <Chatbot />
     </main>
