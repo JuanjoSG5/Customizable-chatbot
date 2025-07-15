e762dceec441402c08ef3fb8f50db5b021025cd8
JuanjoSG5 <113671548+JuanjoSG5@users.noreply.github.com>
2025-07-15 15:15:08 +0200

    doc: added espanish translation to the usage

--- src/utils/crawler.ts
+++ src/utils/crawler.ts
@@ -6,6 +6,7 @@ const app = new FirecrawlApp({apiKey: 'fc-66a2cd455d7b4ce0a54be0e4e6026b19'});
 export async function crawlUrl(url: string, depth: number) {
     return await app.crawlUrl(url, {
         limit: depth,
+        maxDepth: 10,
         scrapeOptions: {
             formats: ["markdown"],
         }
